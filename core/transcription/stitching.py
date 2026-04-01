122c2e1d1fbd31e3864371526e6e361df959ae3d
Chintella-Esq <bbc@chintellalaw.com>
2026-04-01 08:31:25 -0400

    fix punctuation issue
    capitalization was previously addressed between audio chunk boundaries but improper punctuation in the preceding chunk remained but has not been fixed hopefully

--- core/transcription/stitching.py
+++ core/transcription/stitching.py
@@ -2,6 +2,13 @@ import re
 from difflib import SequenceMatcher
 from typing import List, Tuple
 
+ABBREVIATIONS = {
+    "mr", "mrs", "ms", "dr", "prof", "sr", "jr", "st", "ave", "blvd",
+    "dept", "est", "govt", "inc", "corp", "ltd", "co", "vs", "etc",
+    "approx", "dept", "fig", "vol", "no", "gen", "gov", "sgt", "cpl",
+    "pvt", "capt", "lt", "col", "maj", "cmdr", "adm", "rev", "hon",
+}
+
 
 def _normalize_word(word: str) -> str:
     return re.sub(r'[^\w]', '', word).lower()
@@ -9,6 +16,36 @@ def _normalize_word(word: str) -> str:
 
 def _normalize_words(words: List[str]) -> List[str]:
     return [_normalize_word(w) for w in words]
+
+
+def _is_boundary_punctuation_artifact(last_word: str, next_word: str) -> bool:
+    if not last_word or not next_word:
+        return False
+
+    if not re.search(r'[.!?]$', last_word):
+        return False
+
+    stripped = re.sub(r'[.!?]+$', '', last_word)
+    if not stripped:
+        return False
+
+    if stripped.lower() in ABBREVIATIONS:
+        return False
+
+    if re.match(r'^[A-Z]{1,4}$', stripped):
+        return False
+
+    if len(stripped) <= 2 and stripped[0].isupper():
+        return False
+
+    if next_word[0].islower():
+        return True
+
+    return False
+
+
+def _strip_trailing_sentence_punctuation(word: str) -> str:
+    return re.sub(r'[.!?]+$', '', word)
 
 
 def stitch_texts(texts: List[str], had_overlap: List[bool],
@@ -48,6 +85,13 @@ def stitch_texts(texts: List[str], had_overlap: List[bool],
             if (remaining and prev_words
                     and _normalize_word(remaining[0]) == _normalize_word(prev_words[-1])):
                 remaining = remaining[1:]
+
+            if remaining and prev_words:
+                if _is_boundary_punctuation_artifact(prev_words[-1], remaining[0]):
+                    fixed_word = _strip_trailing_sentence_punctuation(prev_words[-1])
+                    prev_words[-1] = fixed_word
+                    result = " ".join(prev_words)
+
             if remaining:
                 result = result + " " + " ".join(remaining)
         else:
